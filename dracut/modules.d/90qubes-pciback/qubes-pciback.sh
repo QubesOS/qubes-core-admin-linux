7ce62cbd984b549a78c09af150ab95ff9a06ca1c
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2015-06-15 03:26:04 +0200

    dracut: support the case when xen-pciback is built-in, not a module
    In such case, we can't set module parameters in initramfs. But we can
    bind the driver to appropriate devices manually - which is also doable
    in case of module.

--- dracut/modules.d/90qubes-pciback/qubes-pciback.sh
+++ dracut/modules.d/90qubes-pciback/qubes-pciback.sh
@@ -3,14 +3,23 @@
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 
 # Find all networking devices currenly installed...
-HIDE_PCI=`lspci -mm -n | grep '^[^ ]* "02'|awk '{ ORS="";print "(" $1 ")";}'`
+HIDE_PCI="`lspci -mm -n | grep '^[^ ]* "02'|awk '{print $1}'`"
 
+# ... and optionally all USB controllers...
 if getargbool 0 rd.qubes.hide_all_usb; then
-    HIDE_PCI=$HIDE_PCI`lspci -mm -n | grep '^[^ ]* "0c03'|awk '{ ORS="";print "(" $1 ")";}'`
+    HIDE_PCI="$HIDE_PCI `lspci -mm -n | grep '^[^ ]* "0c03'|awk '{print $1}'`"
 fi
 
-HIDE_PCI=$HIDE_PCI`getarg rd.qubes.hide_pci | tr ',' '\n'|awk '{ ORS="";print "(" $1 ")";}'`
+HIDE_PCI="$HIDE_PCI `getarg rd.qubes.hide_pci | tr ',' ' '`"
+
+modprobe xen-pciback 2>/dev/null || :
 
 # ... and hide them so that Dom0 doesn't load drivers for them
-modprobe pciback hide=$HIDE_PCI 2> /dev/null || modprobe xen-pciback hide=$HIDE_PCI
-
+for dev in $HIDE_PCI; do
+    BDF=0000:$dev
+    if [ -e /sys/bus/pci/devices/$BDF/driver ]; then
+        echo -n $BDF > /sys/bus/pci/devices/$BDF/driver/unbind
+    fi
+    echo -n $BDF > /sys/bus/pci/drivers/pciback/new_id
+    echo -n $BDF > /sys/bus/pci/drivers/pciback/bind
+done
