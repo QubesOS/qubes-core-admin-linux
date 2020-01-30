8ef229a12e2fed2b393779c26fb23309d5fe2094
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2020-01-30 02:16:53 +0100

    dracut: install cirrus and bochs_drm into initramfs on relevant machines
    Workaround for dracutdevs/dracut#712 causing cirrus and bochs_drm
    modules being skipped when collecting drm drivers.
    
    (cherry picked from commit f98fb82e7565295d048bf0b04c23670dc3679845)

--- dracut/modules.d/90extra-modules/module-setup.sh
+++ dracut/modules.d/90extra-modules/module-setup.sh
@@ -8,4 +8,15 @@ installkernel() {
     hostonly='' instmods xhci-pci xhci-plat-hcd || :
     # ohci-hcd split off
     hostonly='' instmods ohci-pci || :
+    # workaround for https://github.com/dracutdevs/dracut/issues/712
+    if [[ $hostonly ]]; then
+        if [ "$(cat /sys/bus/pci/devices/0000:00:02.0/vendor)" = "0x1013" ]; then
+            hostonly='' instmods cirrus
+        fi
+        if [ "$(cat /sys/bus/pci/devices/0000:00:02.0/vendor)" = "0x1234" ]; then
+            hostonly='' instmods bochs_drm
+        fi
+    else
+        instmods cirrus bochs_drm
+    fi
 }
