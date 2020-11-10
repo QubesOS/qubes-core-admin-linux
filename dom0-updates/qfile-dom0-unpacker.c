bd3de7e6192c7d526f53f2cbb15d859b77c00be2
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2020-11-10 18:06:33 +0100

    dom0-updates: double default max updates bytes
    Add a safety mesure to ensure we have enought place on dom0 rootfs
    
    (cherry picked from commit 02ed544eec9c2ce0f7d55f9717575434c42d4cca)

--- dom0-updates/qfile-dom0-unpacker.c
+++ dom0-updates/qfile-dom0-unpacker.c
@@ -5,15 +5,19 @@
 #include <stdlib.h>
 #include <pwd.h>
 #include <sys/stat.h>
+#include <sys/statvfs.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/fsuid.h>
 #include <errno.h>
 #include <libqubes-rpc-filecopy.h>
 
-#define DEFAULT_MAX_UPDATES_BYTES (2LL<<30)
+#define DEFAULT_MAX_UPDATES_BYTES (4LL<<30)
 #define DEFAULT_MAX_UPDATES_FILES 2048
  
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
 int prepare_creds_return_uid(const char *username)
 {
 	struct passwd *pwd;
@@ -49,13 +53,22 @@ int main(int argc, char ** argv)
 	long long files_limit = DEFAULT_MAX_UPDATES_FILES;
 	long long bytes_limit = DEFAULT_MAX_UPDATES_BYTES;
 
+	struct statvfs st;
+	long long root_free_space;
+
 	if (argc < 3) {
 		fprintf(stderr, "Invalid parameters, usage: %s user dir [-v]\n", argv[0]);
 		exit(1);
 	}
 
+	statvfs("/", &st);
+	// take a little margin with 90% of the free space
+	root_free_space = max(0, st.f_bfree * st.f_frsize * 0.90);
+
+	bytes_limit = min(root_free_space, DEFAULT_MAX_UPDATES_BYTES);
+
 	if ((var=getenv("UPDATES_MAX_BYTES")))
-		bytes_limit = atoll(var);
+		bytes_limit = min(root_free_space, atoll(var));
 	if ((var=getenv("UPDATES_MAX_FILES")))
 		files_limit = atoll(var);
 
