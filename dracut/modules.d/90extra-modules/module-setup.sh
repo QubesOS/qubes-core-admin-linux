40761c1f3f2a053da3efa4a7cbcdc4ca7ad04eed
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2022-01-16 04:16:18 +0100

    dracut: don't print confusing error on AMD systems
    Checking vendor of 00:02.0 device will fail usually on AMD system, which
    is fine - the point of the check is to detect QEMU-emulated device, and
    real AMD isn't one. Hide confusing message.
    
    QubesOS/qubes-issues#7191
    
    (cherry picked from commit 0d811f06b2be39a2c752a3806691e6f3301f0f58)

--- dracut/modules.d/90extra-modules/module-setup.sh
+++ dracut/modules.d/90extra-modules/module-setup.sh
@@ -10,10 +10,10 @@ installkernel() {
     hostonly='' instmods ohci-pci || :
     # workaround for https://github.com/dracutdevs/dracut/issues/712
     if [[ $hostonly ]]; then
-        if [ "$(cat /sys/bus/pci/devices/0000:00:02.0/vendor)" = "0x1013" ]; then
+        if [ "$(cat /sys/bus/pci/devices/0000:00:02.0/vendor 2>/dev/null || :)" = "0x1013" ]; then
             hostonly='' instmods cirrus
         fi
-        if [ "$(cat /sys/bus/pci/devices/0000:00:02.0/vendor)" = "0x1234" ]; then
+        if [ "$(cat /sys/bus/pci/devices/0000:00:02.0/vendor 2>/dev/null || :)" = "0x1234" ]; then
             hostonly='' instmods bochs_drm
         fi
     else
