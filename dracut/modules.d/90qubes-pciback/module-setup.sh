324f5fa105b0ffcdc53c3661dfaa27aa75cd7990
3hhh <tripleh@hackingthe.net>
2022-02-26 09:24:06 +0100

    usbguard: rip out rules.conf
    Only use the rules.d folder to specify rules.
    Reasons:
    a) rules.conf is maintained by upstream Fedora, not Qubes OS.
    b) Changes to rules.conf may come unexpected for users relying on
       rules.d.
    c) The order of rules in rules.conf in combination with rules.d
       is not documented by usbguard and may thus be subject to change.
       This may lead to unexpected behaviour and security issues.
    
    https://github.com/QubesOS/qubes-issues/issues/7248

--- dracut/modules.d/90qubes-pciback/module-setup.sh
+++ dracut/modules.d/90qubes-pciback/module-setup.sh
@@ -8,7 +8,7 @@ install () {
     mkdir -p -m 0700 -- "$initdir/etc/usbguard"
     mkdir -p -m 0755 -- "$systemdsystemunitdir/usbguard.service.d"
     inst_multiple /etc/nsswitch.conf
-    inst_multiple /etc/usbguard/{qubes-usbguard.conf,rules.conf,rules.d,IPCAccessControl.d}
+    inst_multiple /etc/usbguard/{qubes-usbguard.conf,rules.d,IPCAccessControl.d}
     inst_multiple /etc/usbguard/rules.d/*
     inst -l /usr/bin/usbguard
     inst -l /usr/sbin/usbguard-daemon
